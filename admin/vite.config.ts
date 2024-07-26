41f03364f33d6284ade175f6d32649b63737874a
cube00y <lomi1241@gmail.com>
2024-07-26 17:32:16 +0900

    v

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
